a3463745b90271478260da4ea259fd3f3fb78874
Yaseer Jawhar <yaseer.jawhar@vetpartners.co.uk>
2023-04-12 14:50:30 +0100

    initial working version

---
+++ src/HelloWorld.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './HelloWorld.css'
+
+function HelloWorld(){
+    return(
+        <>
+        <h1 className="h1-blue">Hello, World!</h1>
+        < hr/>
+        </>
+    )
+}
+
+export default HelloWorld;
